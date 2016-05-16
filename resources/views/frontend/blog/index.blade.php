ae30d6318e1b279f83437ecb815cfa7087b50bb3
KrulH <mehmetcelik4@gmail.com>
2016-05-16 07:46:19 +0300

    Add  Frontend

---
+++ resources/views/frontend/blog/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title')
+    Blog Index
+@endsection
+
+@section('content')
+    <article><h3>Post Title</h3></article>
+    <span class="subtitle">Post Author | Date</span>
+    <p>Post Body</p>
+    <a href="#">Read more</a>
+
+    Paginations
+
+@endsection
---
+++ resources/views/frontend/blog/single.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+    title
+@endsection
+
+@section('content')
+    <article>
+        <h1>POst title</h1>
+        <span class="subtitle">Post author and date</span>
+        <p> post body</p>
+    </article>
+@endsection
